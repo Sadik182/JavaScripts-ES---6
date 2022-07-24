8a35f7ae5791fd1b50655630364207f8dfb04b79
sadik182 <sadikurrahman728@gmail.com>
2022-07-25 02:58:40 +0600

    array

---
+++ Array.js
@@ -0,0 +1,15 @@
+const costomer = ['m','f', 'm', 'm', 'f', 'm','f', 'm', 'm', 'f'];
+const m = [];
+const f= [];
+
+for(let x of costomer){
+    if(x == 'm')[
+        m.push(x)
+    ]
+    else{
+        f.push(x)
+    }
+}
+console.log(m);
+console.log('Number of Male',m.length);
+console.log(f);
