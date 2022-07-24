af98e8a8c8f6dddbdca8cf13b98ff44906486929
sadik182 <sadikurrahman728@gmail.com>
2022-07-24 16:55:21 +0600

    add some problem

---
+++ export/export.js
@@ -0,0 +1,3 @@
+export default function detail(name, age) {
+    return `Hello This is ${name}, My Age is ${age} year old!`;
+}
---
+++ import.js
@@ -0,0 +1,3 @@
+import detail from './export/export'
+
+console.log(detail('Md. Sadikur Rahman', 25));
