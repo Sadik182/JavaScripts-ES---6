0935cc423c042849cfc2a0bae0105c747ab84ebd
sadik182 <sadikurrahman728@gmail.com>
2022-07-31 20:11:54 +0600

    Functional Programing done

---
+++ Global-Array.js
@@ -0,0 +1,19 @@
+//Refactor Global Variables Out of Functions
+//Rewrite the code so the global array bookList is not changed inside either function. The add function should add the given bookName to the end of the array passed to it and return a new array (list). The remove function should remove the given bookName from the array passed to it.
+
+// The global variable
+const bookList = ["The Hound of the Baskervilles", "On The Electrodynamics of Moving Bodies", "Philosophiæ Naturalis Principia Mathematica", "Disquisitiones Arithmeticae"];
+
+// Change code below this line
+function add(list, bookName) {
+  return [...list, bookName];
+  // Change code above this line
+}
+// Change code below this line
+function remove(list, bookName) {
+    return list.filter(book => book !== bookName);
+    // Change code above this line   
+}
+var newBookList = add(bookList, 'A Brief History of Time');
+var newerBookList = remove(bookList, 'On The Electrodynamics of Moving Bodies');
+var newestBookList = remove(add(bookList, 'A Brief History of Time'), 'On The Electrodynamics of Moving Bodies');
