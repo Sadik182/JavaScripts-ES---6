575ada596c16167de45d5f27a019d42e835f7f99
sadik182 <sadikurrahman728@gmail.com>
2022-08-01 21:22:51 +0600

    array index

--- Basic_Data_Structures/Array-store.js
+++ Basic_Data_Structures/Array-store.js
@@ -1,2 +1,9 @@
 let yourArray = ['Sadik', 1820, true, undefined, null]
 console.log(yourArray.length);
+
+
+let myArray = ["a", "b", "c", "d"];
+// Only change code below this line
+myArray[1] = 'be is not there';
+// Only change code above this line
+console.log(myArray);
