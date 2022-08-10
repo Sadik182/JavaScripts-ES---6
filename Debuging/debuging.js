d16d6e344f10eda8125bc8e78c55ecdbb76f9ab6
sadik182 <sadikurrahman728@gmail.com>
2022-08-10 23:47:04 +0600

    typeof

--- Debuging/debuging.js
+++ Debuging/debuging.js
@@ -11,3 +11,11 @@ console.log(a);
 let output = "Get this to show once in the freeCodeCamp console and not at all in the browser console";
 console.log(output);
 console.clear();
+
+
+let seven = 7;
+let three = "3";
+console.log(seven + three);
+// Only change code below this line
+console.log(typeof(seven))
+console.log(typeof(three))
