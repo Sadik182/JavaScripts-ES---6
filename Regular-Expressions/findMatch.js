903b228194461eae73d3ce48694b9dd82b5e9d74
sadik182 <sadikurrahman728@gmail.com>
2022-08-20 11:38:16 +0600

    more reg expressions

--- Regular-Expressions/findMatch.js
+++ Regular-Expressions/findMatch.js
@@ -6,3 +6,17 @@ let starRegex = /twinkle/ig; // Change this line
 let result = twinkleStar.match(starRegex); // Change this line
 console.log(result);
 
+//Match Single Character with Multiple Possibilities
+
+
+let quoteSample = "Beware of bugs in the above code; I have only proved it correct, not tried it.";
+let vowelRegex = /[aeiou]/ig; // Change this line
+let result = quoteSample.match(vowelRegex); // Change this line
+
+
+//Match Letters of the Alphabet
+
+
+let quoteSample = "The quick brown fox jumps over the lazy dog.";
+let alphabetRegex = /[a-z]/ig; // Change this line
+let result = quoteSample.match(alphabetRegex) // Change this line
