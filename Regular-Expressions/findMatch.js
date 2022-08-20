e1ea20db9e818265cafc8359d5a2df594bc72591
sadik182 <sadikurrahman728@gmail.com>
2022-08-20 11:47:54 +0600

    regex

--- Regular-Expressions/findMatch.js
+++ Regular-Expressions/findMatch.js
@@ -20,3 +20,26 @@ let result = quoteSample.match(vowelRegex); // Change this line
 let quoteSample = "The quick brown fox jumps over the lazy dog.";
 let alphabetRegex = /[a-z]/ig; // Change this line
 let result = quoteSample.match(alphabetRegex) // Change this line
+
+
+//Match Numbers and Letters of the Alphabet
+//Create a single regex that matches a range of letters between h and s, and a range of numbers between 2 and 6. Remember to include the appropriate flags in the regex.
+
+let quoteSample = "Blueberry 3.141592653s are delicious.";
+let myRegex = /[h-s2-6]/ig; // Change this line
+let result = quoteSample.match(myRegex); // Change this line
+
+
+//Match Single Characters Not Specified
+let quoteSample = "3 blind mice.";
+let myRegex = /[^aeiou0-9]/ig; // Change this line
+let result = quoteSample.match(myRegex); // Change this line
+
+
+
+//Match Characters that Occur One or More Times
+//You want to find matches when the letter s occurs one or more times in Mississippi. Write a regex that uses the + sign.
+
+let difficultSpelling = "Mississippi";
+let myRegex = /s+/g; // Change this line
+let result = difficultSpelling.match(myRegex);
