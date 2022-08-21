8360345855252d2c2ed7441aceb201eae0d1d82b
sadik182 <sadikurrahman728@gmail.com>
2022-08-21 21:50:02 +0600

    prototype

--- Object Oriented Programming/OwnProperties.js
+++ Object Oriented Programming/OwnProperties.js
@@ -13,4 +13,58 @@ function Bird(name) {
     }
   }
   console.log(ownProps);
+
+
+  //Use Prototype Properties to Reduce Duplicate Code
+
+  function Dog(name) {
+    this.name = name;
+  }
   
+  Dog.prototype.numLegs = 2;
+  
+  let beagle = new Dog("Snoopy");
+  console.log(beagle.numLegs);
+
+
+
+  //Iterate Over All Properties
+
+
+  function Dog(name) {
+    this.name = name;
+  }
+  
+  Dog.prototype.numLegs = 4;
+  
+  let beagle = new Dog("Snoopy");
+  
+  let ownProps = [];
+  let prototypeProps = [];
+  
+  for(let property in beagle) {
+    if(beagle.hasOwnProperty(property)) {
+      ownProps.push(property)
+    } else {
+      prototypeProps.push(property)
+    }
+  }
+  console.log(ownProps);
+  console.log(prototypeProps);
+
+
+  //Understand the Constructor Property
+
+  function Dog(name) {
+    this.name = name;
+  }
+  
+  // Only change code below this line
+  function joinDogFraternity(candidate) {
+  if(candidate.constructor === Dog) {
+    return true;
+  }
+  else {
+    return false;
+  }
+  }
