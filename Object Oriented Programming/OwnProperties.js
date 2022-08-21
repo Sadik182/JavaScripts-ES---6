b33a321672ecd4424e4de9b3f0fa3101ee9c59bc
sadik182 <sadikurrahman728@gmail.com>
2022-08-21 21:24:49 +0600

    ownProperties

---
+++ Object Oriented Programming/OwnProperties.js
@@ -0,0 +1,16 @@
+//Understand Own Properties
+function Bird(name) {
+    this.name = name;
+    this.numLegs = 2;
+  }
+  
+  let canary = new Bird("Tweety");
+  let ownProps = [];
+  
+  for(let property in canary) {
+    if(canary.hasOwnProperty(property)) {
+      ownProps.push(property);
+    }
+  }
+  console.log(ownProps);
+  
